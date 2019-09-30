2745edadc490851de3bffd98cc42145ae8a3681e
henngelm <henngelm@icloud.com>
2019-09-30 19:52:20 -0300

    rename button declaration

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
-let randomGeneratorButton = document.getElementById("random");
+let randomColorGeneratorButton = document.getElementById("random");
 
 
 // First page load Default values
